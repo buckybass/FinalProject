5bd5f914043ccac3a4d8a793fbee1f41bb5f219d
bass <bassjibass@gmail.com>
2023-03-08 19:28:35 +0700

    add searchbookv1

--- controllers/bookcreate.js
+++ controllers/bookcreate.js
@@ -4,7 +4,6 @@ module.exports = async (req, res) => {
   // JSON Parsing data to object;
   // เพราะมันเป็น String เลยต้องแปลงเป็น Object ก่อน
   const user = JSON.parse(req.body.user)
-  console.log()
   const data = {
     userid: user._id,
     firstname: user.firstname,
@@ -12,10 +11,12 @@ module.exports = async (req, res) => {
     faculty: user.faculty,
     branch: user.branch,
     teacher: req.body.teacher,
+    category: req.body.category,
     bookname: req.body.bookname,
     filename: req.file.filename,
     filepath: req.file.path
   }
+  console.log(data)
   await book.create(data)
   req.flash('success', 'ลงทะเบียนสำเร็จ')
   return res.redirect('/')
--- controllers/search.js
+++ controllers/search.js
@@ -1,7 +1,26 @@
 const book = require('../models/book')
 const teacher = require('../models/Users')
 const category = require('../models/categorys')
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+  const Keyword = req.query.keyword
+  const Category = req.query.category
+  const Teacher = req.query.teacher
+  if (Keyword || Category || Teacher) {
+    console.log(Keyword)
+    book.find({ $or: [{ bookname: Keyword }, { category: Category }, { teacher: Teacher }] }).exec((_err, Book) => {
+      teacher.find({ teacher: true }).exec((_err, Teachers) => {
+        category.find().exec((_err, Categotys) => {
+          res.render('search', {
+            user: req.user,
+            book: Book,
+            teachers: Teachers,
+            categorys: Categotys
+          })
+        })
+      })
+    })
+    return
+  }
   book.find({ approve: true }).exec((_err, Book) => {
     teacher.find({ teacher: true }).exec((_err, Teachers) => {
       category.find().exec((_err, Categotys) => {
