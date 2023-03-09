7177c69861a734d0d2f6afec91f0da02033f56a8
bass <bassjibass@gmail.com>
2023-03-09 14:15:27 +0700

    fix search

--- controllers/search.js
+++ controllers/search.js
@@ -5,30 +5,8 @@ module.exports = async (req, res) => {
   const Keyword = req.query.keyword
   const Category = req.query.category
   const Teacher = req.query.teacher
-  if (Keyword || Category || Teacher) {
-    book
-      .find({
-        $or: [
-          { bookname: Keyword },
-          { category: Category },
-          { teacher: Teacher }
-        ]
-      })
-      .exec((_err, Book) => {
-        teacher.find({ teacher: true }).exec((_err, Teachers) => {
-          category.find().exec((_err, Categotys) => {
-            res.render('search', {
-              user: req.user,
-              book: Book,
-              teachers: Teachers,
-              categorys: Categotys
-            })
-          })
-        })
-      })
-    return
-  }
-  book.find({ approve: true }).exec((_err, Book) => {
+
+  const response = (_err, Book) => {
     teacher.find({ teacher: true }).exec((_err, Teachers) => {
       category.find().exec((_err, Categotys) => {
         res.render('search', {
@@ -39,5 +17,56 @@ module.exports = async (req, res) => {
         })
       })
     })
-  })
+  }
+
+  if (Keyword || Category || Teacher) {
+    if (Keyword && Category) {
+      book
+        .find({
+          approve: true,
+          $and: [
+            { bookname: { $regex: new RegExp(Keyword, 'i') } },
+            { category: Category }
+          ]
+        })
+        .exec(response)
+      return
+    }
+    if (Keyword && Teacher) {
+      book
+        .find({
+          $and: [{ bookname: { $regex: new RegExp(Keyword, 'i') } }, { teacher: Teacher }]
+        })
+        .exec(response)
+      return
+    }
+    if (Category && Teacher) {
+      book
+        .find({
+          $and: [{ category: Category }, { teacher: Teacher }]
+        })
+        .exec(response)
+      return
+    }
+    if (Keyword) {
+      book
+        .find({
+          $and: [{ bookname: { $regex: new RegExp(Keyword, 'i') } }]
+        })
+        .exec(response)
+      return
+    }
+    book
+      .find({
+        approve: true,
+        $or: [
+          // { bookname: { $regex: Keyword } },
+          { category: Category },
+          { teacher: Teacher }
+        ]
+      })
+      .exec(response)
+    return
+  }
+  book.find({ approve: true }).exec(response)
 }
