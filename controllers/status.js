073af37d6e52f28157f8af1896726918b90c7253
bass <bassjibass@gmail.com>
2023-03-13 20:16:49 +0700

    test case sensirive

--- controllers/status.js
+++ controllers/status.js
@@ -1,11 +1,11 @@
 const book = require('../models/book')
-const bookrefer = require('../models/bookRefer')
+const bookRefer = require('../models/bookrefer')
 const bookdowload = require('../models/bookDowload')
 
 module.exports = (req, res) => {
   book.find({ userid: req.user.id }).exec((_err, Books) => {
     if (Books[0]) {
-      bookrefer.countDocuments({ bookid: Books[0]._id }).exec((_err, CountRefer) => {
+      bookRefer.countDocuments({ bookid: Books[0]._id }).exec((_err, CountRefer) => {
         bookdowload.countDocuments({ bookid: Books[0]._id }).exec((_err, CountDowload) => {
           res.render('status', {
             user: req.user,
